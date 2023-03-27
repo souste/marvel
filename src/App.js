26e4b914d90a635494a139a048e9397cba80793c
Stephen <stephensouness@outlook.com>
2023-03-27 15:52:05 +0100

    got the marvel api working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import SearchBar from "./SearchBar";
+import { useState } from "react";
+import searchAPIs from "./api";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+  const handleSubmit = async (term) => {
+    const result = await searchAPIs.searchCharacters(term);
+    setCharacters(result);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SearchBar onSubmit={handleSubmit} />
     </div>
   );
 }
